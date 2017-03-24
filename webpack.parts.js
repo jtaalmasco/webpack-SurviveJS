78302c067b1a04d4b4d1a7beb3a037410eb52e98
Jerred Almasco <jtaalmasco@gmail.com>
2017-03-24 11:02:01 +0800

    Added webpack-merge

---
+++ webpack.parts.js
@@ -0,0 +1,26 @@
+exports.devServer = ({ host, port } = {}) => ({
+  devServer: {
+    historyApiFallback: true,
+    stats: 'errors-only',
+    host, // Defaults to `localhost`
+    port, // Defaults to 8080
+    overlay: {
+      errors: true,
+      warnings: true,
+    },
+  },
+});
+
+exports.loadCSS = ({ include, exclude } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        include,
+        exclude,
+
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+});
